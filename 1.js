6e9c07e7fcf61e6c86a40116214fe07b8fcc067b
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-02-08 22:34:32 +0600

    color size

--- 1.js
+++ 1.js
@@ -41,8 +41,31 @@ marginbutton.innerText = "marginButton";
 marginbutton.addEventListener('click', function(){
     num --;
     h1.style.marginBottom = num + "5px"
+});
+
+
+
+let color = document.querySelector('.color');
+color.innerText = "color"
+color.addEventListener('click', function(){
+    h1.style.color = "#" + (Math.random().toString(16) + 000000).substring(2,8).toUpperCase()
 })
 
+let size = document.querySelector('.si');
+size.innerText = 'Size +';
+
+size.addEventListener('click', function(){
+    num ++;
+    h1.style.fontSize = num + "8px"
+})
+
+
+let size1 = document.querySelector('.si2');
+size1.innerText = "Size - ";
+size1.addEventListener('clock', function(){
+    num --;
+    h1.style.fontSize = num + "1px"
+})
 
 
 
